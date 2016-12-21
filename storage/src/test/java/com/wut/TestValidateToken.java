91a52fe4e204862a95e620ea15e1bcdbb52bec77
annguyen-qh <annguyen.qh@gmail.com>
2016-12-21 17:30:27 +0700

    make checkToken funtion be public

---
+++ storage/src/test/java/com/wut/TestValidateToken.java
@@ -0,0 +1,14 @@
+package com.wut;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestValidateToken {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
