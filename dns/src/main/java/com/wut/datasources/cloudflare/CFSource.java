853ab189d3efade8a32ca41727e925030f83f821
annguyen-qh <annguyen.qh@gmail.com>
2017-01-22 18:46:41 +0700

    TENDOFFICE-3207: Create page rules when creating DNS record

--- dns/src/main/java/com/wut/datasources/cloudflare/CFSource.java
+++ dns/src/main/java/com/wut/datasources/cloudflare/CFSource.java
@@ -3,23 +3,25 @@ package com.wut.datasources.cloudflare;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map.Entry;
 
 import com.wut.model.Data;
 import com.wut.model.map.MappedData;
 import com.wut.model.map.MessageData;
 import com.wut.model.message.ErrorData;
+import com.wut.model.scalar.IntegerData;
+import com.wut.model.scalar.ScalarData;
+import com.wut.model.scalar.StringData;
 import com.wut.support.domain.DomainUtils;
 import com.wut.support.settings.SettingsManager;
 
@@ -37,68 +39,68 @@ public class CFSource {
 		cloudflareAuth = new CFAuth(key, email);
 	}
 
-	public Data createZone(String customerDomain) throws UnsupportedOperationException {
-		// Send request
+	public Data createZone(String customerDomain) {
 		JsonArray nsArray = new JsonArray();
-		HttpPost postReq = new HttpPost(CDNUtils.createZoneEndpoint());
-		CDNUtils.setCFHeader(postReq, cloudflareAuth);
-		JsonObject postData = CDNUtils.setCreateZoneData(DomainUtils.getTopLevelDomain(customerDomain));
-		postReq.setEntity(new StringEntity(postData.toString(), "UTF-8"));
-		CloseableHttpClient client = HttpClients.createDefault();
+		HttpPost postReq = new HttpPost(CFUtils.createZoneEndpoint());
+		CFUtils.setCFHeader(postReq, cloudflareAuth);
+
+		JsonObject postData = CFUtils.setCreateZoneData(DomainUtils.getTopLevelDomain(customerDomain));
+		CFUtils.setBody(postReq, postData);
+
+		CFResponse cfResponse;
 		try {
+			CloseableHttpClient client = HttpClients.createDefault();
 			CloseableHttpResponse response = client.execute(postReq);
-			JsonObject cfReturn = CDNUtils.parseCFResponse(response);
-			if (cfReturn.get("success").getAsString().equals("true")) {
-				JsonObject cfResult = (JsonObject) cfReturn.get("result");
-				nsArray = (JsonArray) cfResult.get("name_servers");
-			} else {
-				JsonArray cfError = (JsonArray) cfReturn.get("errors");
-				JsonObject cfErrordetail = cfError.get(0).getAsJsonObject();
-				int errorCode = cfErrordetail.get("code").getAsInt();
-				switch (errorCode) {
-				case 1049: // this domain is not a registered domain
-					return ErrorData.INVALID_DOMAIN;
-				case 1061: // domain already exists
-				case 90: // already exists but CF mark as temporarily banned
-							// (perhaps because we tried to add too many times)
-					nsArray = getZoneNameServer(DomainUtils.getTopLevelDomain(customerDomain));
-				default:
-					break;
-				}
-			}
-			if (nsArray != null && nsArray.size() == 2) {
-				MappedData ret = new MappedData();
-				ret.put("ns1", nsArray.get(0).getAsString());
-				ret.put("ns2", nsArray.get(1).getAsString());
-				return ret;
-			} else {
-				return MessageData.error(cfReturn.get("errors").toString());
-			}
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-			return MessageData.error(e);
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-			return MessageData.error(e);
+			cfResponse = new CFResponse(response);
 		} catch (IOException e) {
 			e.printStackTrace();
 			return MessageData.error(e);
 		}
+
+		if (cfResponse.isSuccess()) {
+			JsonObject cfResult = (JsonObject) cfResponse.getCFReturn().get("result");
+			nsArray = (JsonArray) cfResult.get("name_servers");
+		} else {
+			JsonObject cfError = cfResponse.getError();
+			int errorCode = cfError.get("code").getAsInt();
+			switch (errorCode) {
+			case 1049: // this domain is not a registered domain
+				return ErrorData.INVALID_DOMAIN;
+			case 1061: // domain already exists
+			case 90: // already exists but CF mark as temporarily banned
+						// (perhaps because we tried to add too many times)
+				nsArray = getZoneNameServer(DomainUtils.getTopLevelDomain(customerDomain));
+			default:
+				break;
+			}
+		}
+		if (nsArray != null && nsArray.size() == 2) {
+			MappedData ret = new MappedData();			
+			ret.put("ns1", nsArray.get(0).getAsString());
+			ret.put("ns2", nsArray.get(1).getAsString());			
+			return ret;
+		} else {
+			JsonObject cfError = cfResponse.getError();
+			return CFUtils.returnCFMessage(cfError);
+		}
 	}
 
 	public JsonArray getZoneNameServer(String customerDomain) {
-		// Send request
-		HttpGet getReq = new HttpGet(CDNUtils.listZoneEndpoint());
-		CDNUtils.setCFHeader(getReq, cloudflareAuth);
-		CloseableHttpClient client = HttpClients.createDefault();
+		HttpGet getReq = new HttpGet(CFUtils.listZoneEndpoint());
+		CFUtils.setCFHeader(getReq, cloudflareAuth);
+
+		CFResponse cfResponse;
 		try {
-			HttpResponse response = client.execute(getReq);
-			JsonObject cfReturn = CDNUtils.parseCFResponse(response);
-			if (!cfReturn.has("success") || !cfReturn.get("success").getAsString().equals("true")) {
-				System.out.println("getZoneNameServer error" + cfReturn.get("errors").toString());
-				return null;
-			}
-			JsonArray cfResult = (JsonArray) cfReturn.get("result");
+			CloseableHttpClient client = HttpClients.createDefault();
+			CloseableHttpResponse response = client.execute(getReq);
+			cfResponse = new CFResponse(response);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		if (cfResponse.isSuccess()) {
+			JsonArray cfResult = (JsonArray) cfResponse.getCFReturn().get("result");
 			for (Object zone : cfResult) {
 				if (zone instanceof JsonObject) {
 					JsonObject item = (JsonObject) zone;
@@ -108,32 +110,28 @@ public class CFSource {
 				}
 			}
 			return null;
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-			return null;
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-			return null;
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.out.print(e.getMessage());
+		} else {
+			System.out.println(cfResponse.getError().toString());
 			return null;
 		}
 	}
 
 	public String getZoneId(String customerDomain) {
-		// Send request
-		HttpGet getReq = new HttpGet(CDNUtils.listZoneEndpoint());
-		CDNUtils.setCFHeader(getReq, cloudflareAuth);
-		CloseableHttpClient client = HttpClients.createDefault();
+		HttpGet getReq = new HttpGet(CFUtils.listZoneEndpoint());
+		CFUtils.setCFHeader(getReq, cloudflareAuth);
+
+		CFResponse cfResponse;
 		try {
-			HttpResponse response = client.execute(getReq);
-			JsonObject cfReturn = CDNUtils.parseCFResponse(response);
-			if (!cfReturn.has("success") || !cfReturn.get("success").getAsString().equals("true")) {
-				System.out.println("getZoneID error" + cfReturn.get("errors").toString());
-				return null;
-			}
-			JsonArray cfResult = (JsonArray) cfReturn.get("result");
+			CloseableHttpClient client = HttpClients.createDefault();
+			CloseableHttpResponse response = client.execute(getReq);
+			cfResponse = new CFResponse(response);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		if (cfResponse.isSuccess()) {
+			JsonArray cfResult = (JsonArray) cfResponse.getCFReturn().get("result");
 			for (Object zone : cfResult) {
 				if (zone instanceof JsonObject) {
 					JsonObject item = (JsonObject) zone;
@@ -143,32 +141,28 @@ public class CFSource {
 				}
 			}
 			return null;
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-			return null;
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-			return null;
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.out.print(e.getMessage());
+		} else {
+			System.out.println(cfResponse.getError().toString());
 			return null;
 		}
 	}
 
 	public String getRecordId(String zoneID, String customerDomain, String recordName) {
-		// Send request
-		HttpGet getReq = new HttpGet(CDNUtils.listRecordEndpoint(zoneID));
-		CDNUtils.setCFHeader(getReq, cloudflareAuth);
-		CloseableHttpClient client = HttpClients.createDefault();
+		HttpGet getReq = new HttpGet(CFUtils.listRecordEndpoint(zoneID));
+		CFUtils.setCFHeader(getReq, cloudflareAuth);
+
+		CFResponse cfResponse;
 		try {
-			HttpResponse response = client.execute(getReq);
-			JsonObject cfReturn = CDNUtils.parseCFResponse(response);
-			if (!cfReturn.has("success") || !cfReturn.get("success").getAsString().equals("true")) {
-				System.out.println("getRecordId error" + cfReturn.get("errors").toString());
-				return null;
-			}
-			JsonArray cfResult = (JsonArray) cfReturn.get("result");
+			CloseableHttpClient client = HttpClients.createDefault();
+			CloseableHttpResponse response = client.execute(getReq);
+			cfResponse = new CFResponse(response);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		if (cfResponse.isSuccess()) {
+			JsonArray cfResult = (JsonArray) cfResponse.getCFReturn().get("result");
 			for (Object zone : cfResult) {
 				if (zone instanceof JsonObject) {
 					JsonObject item = (JsonObject) zone;
@@ -179,112 +173,170 @@ public class CFSource {
 				}
 			}
 			return null;
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-			return null;
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-			return null;
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.out.print(e.getMessage());
+		} else {
+			System.out.println(cfResponse.getError().toString());
 			return null;
 		}
 	}
 
 	public Data getRecordDetails(String customerDomain, String recordName) {
-		// Send request
 		String zoneId = getZoneId(customerDomain);
 		String recordId = getRecordId(zoneId, customerDomain, recordName);
-		HttpGet getReq = new HttpGet(CDNUtils.detailRecordEndpoint(zoneId, recordId, recordName));
-		CDNUtils.setCFHeader(getReq, cloudflareAuth);
-		CloseableHttpClient client = HttpClients.createDefault();
-		MappedData ret = new MappedData();
+
+		HttpGet getReq = new HttpGet(CFUtils.detailRecordEndpoint(zoneId, recordId));
+		CFUtils.setCFHeader(getReq, cloudflareAuth);
+
+		CFResponse cfResponse;
 		try {
-			HttpResponse response = client.execute(getReq);
-			JsonObject cfReturn = CDNUtils.parseCFResponse(response);
-			System.out.println(getReq.getURI());
-			if (!cfReturn.has("success") || !cfReturn.get("success").getAsString().equals("true")) {
-				System.out.println("getRecordDetails error" + cfReturn.get("errors").toString());
-				return null;
-			}
-			System.out.println(cfReturn.get("result"));
-			JsonObject cfResult = (JsonObject) cfReturn.get("result");
+			CloseableHttpClient client = HttpClients.createDefault();
+			CloseableHttpResponse response = client.execute(getReq);
+			cfResponse = new CFResponse(response);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return MessageData.error(e);
+		}
+
+		if (cfResponse.isSuccess()) {
+			MappedData ret = new MappedData();
+			JsonObject cfResult = (JsonObject) cfResponse.getCFReturn().get("result");
 			for (Entry<String, JsonElement> entry : cfResult.entrySet()) {
 				if (!entry.getValue().isJsonObject()) {
 					ret.put(entry.getKey().toString(), entry.getValue().getAsString());
 				}
 			}
 			return ret;
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-			return null;
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-			return null;
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.out.print(e.getMessage());
-			return null;
+		} else {
+			JsonObject cfError = cfResponse.getError();
+			return CFUtils.returnCFMessage(cfError);
 		}
 	}
 
-	public Data createRecord(String customerDomain, String recordName, String content)
-			throws UnsupportedOperationException {
-		// Send request
+	public Data createRecord(String customerDomain, String recordName, String content) {
 		String zoneId = getZoneId(customerDomain);
-		HttpPost postReq = new HttpPost(CDNUtils.createRecordEndpoint(zoneId));
-		CDNUtils.setCFHeader(postReq, cloudflareAuth);
-		JsonObject postData = CDNUtils.setRecordData(recordName, content);
-		postReq.setEntity(new StringEntity(postData.toString(), "UTF-8"));
-		CloseableHttpClient client = HttpClients.createDefault();
+
+		HttpPost postReq = new HttpPost(CFUtils.createRecordEndpoint(zoneId));
+		CFUtils.setCFHeader(postReq, cloudflareAuth);
+
+		JsonObject postData = CFUtils.setRecordData(recordName, content);
+		CFUtils.setBody(postReq, postData);
+
+		CFResponse cfResponse;
 		try {
+			CloseableHttpClient client = HttpClients.createDefault();
 			CloseableHttpResponse response = client.execute(postReq);
-			JsonObject cfReturn = CDNUtils.parseCFResponse(response);
-			if (cfReturn.get("success").getAsString().equals("true")) {
-				return MessageData.success();
-			} else {
-				return MessageData.error(cfReturn.get("errors").toString());
-			}
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-			return MessageData.error(e);
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-			return MessageData.error(e);
+			cfResponse = new CFResponse(response);
 		} catch (IOException e) {
 			e.printStackTrace();
 			return MessageData.error(e);
 		}
+
+		if (cfResponse.isSuccess()) {
+			if (recordName.equals("www")) {
+				List<PageRule> defaultPagerules = defaultPagerules(customerDomain);
+				for (PageRule rule : defaultPagerules) {
+					createPagerule(customerDomain, rule);
+				}
+			}
+			return MessageData.success();
+		}
+
+		JsonObject cfError = cfResponse.getError();
+		int errorCode = cfError.get("code").getAsInt();
+		switch (errorCode) {
+		case 81053: // record already exists
+			return ErrorData.RECORD_EXISTED;
+		case 1004:
+			return ErrorData.RECORD_INVALID;
+		default:
+			return CFUtils.returnCFMessage(cfError);
+		}
 	}
 
-	public Data updateRecord(String customerDomain, String recordName, String content)
-			throws UnsupportedOperationException {
-		// Send request
+	public Data updateRecord(String customerDomain, String recordName, String content) {
 		String zoneId = getZoneId(customerDomain);
 		String recordId = getRecordId(zoneId, customerDomain, recordName);
-		HttpPutWithBody postReq = new HttpPutWithBody(CDNUtils.updateRecordEndpoint(zoneId, recordId));
-		CDNUtils.setCFHeader(postReq, cloudflareAuth);
-		JsonObject postData = CDNUtils.setRecordData(recordName, content);
-		postReq.setEntity(new StringEntity(postData.toString(), "UTF-8"));
-		CloseableHttpClient client = HttpClients.createDefault();
+
+		HttpPutWithBody putReq = new HttpPutWithBody(CFUtils.updateRecordEndpoint(zoneId, recordId));
+		CFUtils.setCFHeader(putReq, cloudflareAuth);
+
+		JsonObject postData = CFUtils.setRecordData(recordName, content);
+		CFUtils.setBody(putReq, postData);
+
+		CFResponse cfResponse;
 		try {
-			CloseableHttpResponse response = client.execute(postReq);
-			JsonObject cfReturn = CDNUtils.parseCFResponse(response);
-			if (cfReturn.get("success").getAsString().equals("true")) {
-				return MessageData.success();
-			} else {
-				return MessageData.error(cfReturn.get("errors").toString());
-			}
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-			return MessageData.error(e);
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-			return MessageData.error(e);
+			CloseableHttpClient client = HttpClients.createDefault();
+			CloseableHttpResponse response = client.execute(putReq);
+			cfResponse = new CFResponse(response);
 		} catch (IOException e) {
 			e.printStackTrace();
 			return MessageData.error(e);
 		}
+
+		if (cfResponse.isSuccess()) {
+			return MessageData.success();
+		} else {
+			JsonObject cfError = cfResponse.getError();
+			return CFUtils.returnCFMessage(cfError);
+		}
+	}
+
+	public String createPagerule(String customerDomain, PageRule rule) {
+		String zoneId = getZoneId(customerDomain);
+		HttpPost postReq = new HttpPost(CFUtils.createPageRuleEndpoint(zoneId));
+		CFUtils.setCFHeader(postReq, cloudflareAuth);
+
+		JsonObject postData = CFUtils.setPageRulesData(rule);
+		System.out.println(postData.toString());
+		CFUtils.setBody(postReq, postData);
+
+		CFResponse cfResponse;
+		try {
+			CloseableHttpClient client = HttpClients.createDefault();
+			CloseableHttpResponse response = client.execute(postReq);
+			cfResponse = new CFResponse(response);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		if (cfResponse.isSuccess()) {
+			JsonObject cfResult = (JsonObject) cfResponse.getCFReturn().get("result");
+			return cfResult.get("id").toString();
+		} else {
+			return null;
+		}
+	}
+
+	private List<PageRule> defaultPagerules(String customerDomain) {
+		List<PageRule> pagerules = new ArrayList<PageRule>();
+		Action cacheAction1 = new Action("browser_cache_ttl", new IntegerData(86400));// 1 day
+		Action cacheAction2 = new Action("cache_level", new StringData("cache_everything"));
+		String cacheURLPattern = String.format("https://www.%s/*", normalizeDomain(customerDomain));
+
+		pagerules.add(new PageRule(cacheURLPattern, new Action[] { cacheAction1, cacheAction2 }));
+
+		MappedData fwData = new MappedData();
+		fwData.put("url", String.format("https://www.%s/$1", normalizeDomain(customerDomain)));
+		fwData.put("status_code", new IntegerData(301));
+		Action fwAction = new Action("forwarding_url", fwData);
+		String fwURLPattern = String.format("https://%s/*", normalizeDomain(customerDomain));
+
+		pagerules.add(new PageRule(fwURLPattern, new Action[] { fwAction }));
+		
+		// The "Always Use HTTPS" option will only appear if your zone has an active SSL certificate associated with it on Cloudflare
+//		Action httpsAction = new Action("always_use_https", new StringData("on"));
+//		String httpsURLPattern = String.format("https://www.%s/*", normalizeDomain(customerDomain));
+		MappedData httpsData = new MappedData();
+		httpsData.put("url", String.format("https://www.%s/$1", normalizeDomain(customerDomain)));
+		httpsData.put("status_code", new IntegerData(301));
+		Action httpsAction = new Action("forwarding_url", httpsData);
+		String httpsURLPattern = String.format("http://www.%s/*", normalizeDomain(customerDomain));
+
+		pagerules.add(new PageRule(httpsURLPattern, new Action[] { httpsAction }));
+		return pagerules;
+	}
+
+	public String normalizeDomain(String domainName) {
+		return domainName.startsWith("www.") ? domainName.substring(4) : domainName;
 	}
 }
